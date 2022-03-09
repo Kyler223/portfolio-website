67918d81f358238645ded860ae622783bbdf274c
RomeTwinn <rometwinn@gmail.com>
2022-03-09 11:35:39 -0600

    iframe added that auto scrolls

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ function hamburgerClick() {
     navlinks.classList.toggle('navLinksDropdown');
     navItem.forEach(item => {item.classList.toggle('navItemDropdown')});
 }
+
+var height = -70;
+var hover = false;
+
+setInterval(function() {
+    var iframe = document.getElementById('bannerIframe');
+    if(!hover) {
+        iframe.contentWindow.scrollTo(0, height);
+        height = height + .3;
+    }
+    else {
+        height = iframe.contentWindow.pageYOffset;
+    }
+
+    if(height > iframe.contentWindow.pageYOffset + 100) {
+        height = -50;
+    }
+
+}, 5);
+
+function iframeMouseEnter() {
+    hover = true;
+}
+
+function iframeMouseOut() {
+    hover = false;
+}
+
