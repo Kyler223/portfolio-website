02a39008d99c6886a0fe5e3e9f4cf421344f5749
RomeTwinn <rometwinn@gmail.com>
2022-03-24 15:35:56 -0500

    centered the menu icon when clicked

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function hamburgerClick() {
     //stating varibles do not work outside the fuction
+    var hamburger = document.getElementById('hamburger');
     var navlinks = document.getElementById('nav-links');
     var navItem = document.querySelectorAll('.nav-item');
     var contactDropdown = document.getElementById('contactDropdown');
 
+    classToggler(hamburger, 'hamburger-center');
+    classToggler(navlinks, 'navLinksDropdown');
+    navItem.forEach(item => {classToggler(item, 'navItemDropdown')});
     contactDropdown.textContent = 'Contact';
-    navlinks.classList.toggle('navLinksDropdown');
-    navItem.forEach(item => {item.classList.toggle('navItemDropdown')});
+}
+
+function classToggler(element, className) {
+    element.classList.toggle(className);
 }
