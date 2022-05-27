b0ca58838cb8d5e411e200635cd9c755e78a6d59
RomeTwinn <rometwinn@gmail.com>
2022-05-27 17:18:13 -0500

    no more intersection observers for images

--- lazy-load.js
+++ lazy-load.js
@@ -1,30 +1,17 @@
 const targetImgs = document.querySelectorAll('img');
 const targetIframes = document.querySelectorAll('iframe');
 
-const lazyLoadImg = target => {
-    const io = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
+const lazyLoad = target => {
+    const src = target.getAttribute('lazy');
+    const srcset = target.getAttribute('srcset-lazy');
 
-        if (entry.isIntersecting) {
-            const img = entry.target;
-            const src = img.getAttribute('lazy');
-            const srcset = img.getAttribute('srcset-lazy');
-
-            if(src) {
-                console.log(`src: ${src}`);
-                img.setAttribute('src', src);
-            }
-            if(srcset) {
-                console.log(`srcset: ${srcset}`);
-                img.setAttribute('srcset', srcset);
-            }
-
-            observer.disconnect();
-        }
-        });
-    });
-io.observe(target)
+    if(src) {
+        target.setAttribute('src', src);
+    }
+    if(srcset) {
+        target.setAttribute('srcset', srcset);
+    }
 };
 
-targetImgs.forEach(lazyLoadImg);
-targetIframes.forEach(lazyLoadImg);
+targetImgs.forEach(lazyLoad);
+targetIframes.forEach(lazyLoad);
