26be1c9f3a64be4a8fed3fefe15e6e06fc7a4842
RomeTwinn <rometwinn@gmail.com>
2022-05-30 14:23:58 -0500

    slowed scrolling

--- iframe-scroller.js
+++ iframe-scroller.js
@@ -1,5 +1,5 @@
 //the time it waits before page scrolls from the top
-var waitBeforePageScrolls = -50;
+var waitBeforePageScrolls = -100;
 //the time it waits when it hits the end of the page before looping back to the top
 var waitBeforePageTop = 50;
 //current height of the page
@@ -9,7 +9,7 @@ var hover = false;
 //the time it takes to scroll again in milliseconds
 var scrollInterval = 5;
 //how much it scrolls per interval
-var scrollAmount = .3;
+var scrollAmount = .26;
 
 setInterval(function() {
     var iframe = document.getElementById('bannerIframe');
